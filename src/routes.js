2071d56bca4c857fd6424821fc06b122c5f264fe
Gabriel Bianchessi <gabriel.bianchessi@grupow.com.br>
2023-09-06 14:16:45 -0300

    finished module 1

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,27 @@
 import { Database } from "./database.js"
 import { randomUUID } from 'node:crypto'
+import { buildRoutePath } from "./utils/build-route-path.js"
 
 const database = new Database
 
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
-      const users = database.select('users')
+      const { search } = req.query
+
+      const users = database.select('users', search ? {
+        name: search,
+        email: search,
+      } : null)
 
       return res.end(JSON.stringify(users))
     }
   },
   {
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const { name, email } = req.body 
 
@@ -32,9 +38,25 @@ export const routes = [
   },
   {
     method: 'DELETE',
-    path: '/users',
+    path: buildRoutePath('/users/:id'),
     handler: (req, res) => {
-      res.end()
+      const { id } = req.params
+      
+      database.delete('users', id)
+
+      res.writeHead(204).end()
+    }
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { name, email } = req.body
+      
+      database.update('users', id, { name, email })
+
+      res.writeHead(204).end()
     }
   }
 ]
